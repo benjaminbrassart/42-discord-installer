fe462c9c89897949325f68132d8488359f4d6335
Benjamin BRASSART <bbrassar@student.42.fr>
2023-02-14 03:47:41 +0100

    Improve whole script

--- install-discord.sh
+++ install-discord.sh
@@ -1,11 +1,51 @@
 #!/bin/sh
 
-cd /tmp
-curl -Lo discord.tgz https://dl.discordapp.net/apps/linux/0.0.15/discord-0.0.15.tar.gz
-tar xf discord.tgz
-sed -i '/^Exec=/d' Discord/discord.desktop
-echo "Exec=$HOME/.local/Discord/Discord" >> Discord/discord.desktop
-mv Discord $HOME/.local/Discord
+set -ex
 
-cd $HOME/.local/Discord
-mv discord.desktop $HOME/.local/share/applications/discord.desktop
+DISCORD_VERSION='0.0.25'
+TAR_NAME="$(mktemp)"
+
+# change this as you need
+INSTALL_DIR="/sgoinfre/goinfre/Perso/${USER}/Discord"
+INSTALL_DATE="$(date)"
+
+# create installation directory
+mkdir -p "${INSTALL_DIR}"
+
+# check installation directory
+if [ -d "${INSTALL_DIR}/${DISCORD_VERSION}" ]; then
+    printf -- 'Installation directory already exists.\n'
+    exit 1
+fi
+
+TMP_DIR="$(mktemp -d)"
+cd "${TMP_DIR}"
+
+# download archive
+curl -Lo "${TAR_NAME}" "https://dl.discordapp.net/apps/linux/${DISCORD_VERSION}/discord-${DISCORD_VERSION}.tar.gz"
+
+# extract archive
+tar xf "${TAR_NAME}"
+rm -f "${TAR_NAME}"
+
+# move extracted archive to installation directory
+mv ./Discord "${INSTALL_DIR}/${DISCORD_VERSION}"
+
+# create desktop entry directory
+mkdir -p "${HOME}/.local/share/applications"
+
+# create desktop entry
+cat > "${HOME}/.local/share/applications/discord.desktop" << EOF
+[Desktop Entry]
+Name=Discord ${DISCORD_VERSION}
+StartupWMClass=discord
+Comment=All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.
+GenericName=Internet Messenger
+Exec=${INSTALL_DIR}/${DISCORD_VERSION}/Discord
+Icon=${INSTALL_DIR}/${DISCORD_VERSION}/discord.png
+Type=Application
+Categories=Network;InstantMessaging;
+
+# installed with install-discord.sh
+# ${INSTALL_DATE}
+EOF
