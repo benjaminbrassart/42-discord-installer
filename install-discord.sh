2cf46e89847887c88d4ea345b0ed57e2f7c3eb06
Benjamin BRASSART <bbrassar@student.42.fr>
2023-02-14 06:17:07 +0100

    Add environment variable overriding

--- install-discord.sh
+++ install-discord.sh
@@ -2,18 +2,27 @@
 
 set -e
 
-DISCORD_VERSION='0.0.25'
+LATEST_DISCORD_VERSION='0.0.25'
+DEFAULT_INSTALL_DIR="/sgoinfre/goinfre/Perso/${USER}/Discord"
+
+if [ -z "${DISCORD_VERSION}" ]; then
+    DISCORD_VERSION="${LATEST_DISCORD_VERSION}"
+fi
+
+if [ -z "${DISCORD_INSTALL_DIR}" ]; then
+    DISCORD_INSTALL_DIR="${DEFAULT_INSTALL_DIR}"
+fi
+
 TAR_NAME="$(mktemp)"
 
 # change this as you need
-INSTALL_DIR="/sgoinfre/goinfre/Perso/${USER}/Discord"
 INSTALL_DATE="$(date)"
 
 # create installation directory
-mkdir -p "${INSTALL_DIR}"
+mkdir -p "${DISCORD_INSTALL_DIR}"
 
 # check installation directory
-if [ -d "${INSTALL_DIR}/${DISCORD_VERSION}" ]; then
+if [ -d "${DISCORD_INSTALL_DIR}/${DISCORD_VERSION}" ]; then
     printf -- 'Installation directory already exists.\n'
     exit 1
 fi
@@ -22,27 +31,27 @@ TMP_DIR="$(mktemp -d)"
 cd "${TMP_DIR}"
 
 # download archive
-curl -Lo "${TAR_NAME}" "https://dl.discordapp.net/apps/linux/${DISCORD_VERSION}/discord-${DISCORD_VERSION}.tar.gz"
+curl -SsL -o "${TAR_NAME}" "https://dl.discordapp.net/apps/linux/${DISCORD_VERSION}/discord-${DISCORD_VERSION}.tar.gz"
 
 # extract archive
 tar xf "${TAR_NAME}"
 rm -f "${TAR_NAME}"
 
 # move extracted archive to installation directory
-mv ./Discord "${INSTALL_DIR}/${DISCORD_VERSION}"
+mv ./Discord "${DISCORD_INSTALL_DIR}/${DISCORD_VERSION}"
 
 # create desktop entry directory
 mkdir -p "${HOME}/.local/share/applications"
 
 # create desktop entry
-cat > "${HOME}/.local/share/applications/discord.desktop" << EOF
+cat > "${HOME}/.local/share/applications/discord-${DISCORD_VERSION}.desktop" << EOF
 [Desktop Entry]
 Name=Discord ${DISCORD_VERSION}
 StartupWMClass=discord
 Comment=All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.
 GenericName=Internet Messenger
-Exec=${INSTALL_DIR}/${DISCORD_VERSION}/Discord
-Icon=${INSTALL_DIR}/${DISCORD_VERSION}/discord.png
+Exec=${DISCORD_INSTALL_DIR}/${DISCORD_VERSION}/Discord
+Icon=${DISCORD_INSTALL_DIR}/${DISCORD_VERSION}/discord.png
 Type=Application
 Categories=Network;InstantMessaging;
 
